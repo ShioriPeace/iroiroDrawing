0ea3374d3933e3acf3fc0c3f7d5acfa8e8246f16
Shiori Nakayama <shiori.nakayama@pepabo.com>
2019-03-18 18:52:36 +0900

    touchの動作を実装できるメソッドを導入

--- app/src/main/java/com/shiorin/iroiroDrawing/MainActivity.kt
+++ app/src/main/java/com/shiorin/iroiroDrawing/MainActivity.kt
@@ -17,11 +17,14 @@ import android.os.HandlerThread
 import android.widget.Button
 import android.util.Log
 import android.util.Size
+import android.view.MotionEvent
 import android.view.Surface
 import android.view.TextureView
+import android.view.View
 import android.widget.ImageButton
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -30,16 +33,16 @@ class MainActivity : AppCompatActivity() {
         const val PERMISSION_READ_STORAGE = 1001
     }
 
-    private  lateinit var textureView: TextureView
+    private lateinit var textureView: TextureView
     private var imageReader: ImageReader? = null
     private var cameraDevice: CameraDevice? = null
-    private val previewSize: Size = Size(300,300)
+    private val previewSize: Size = Size(300, 300)
 
-    private lateinit var previewRequestBuilder : CaptureRequest.Builder
-    private lateinit var previewRequest : CaptureRequest
-    private lateinit var captureSession : CameraCaptureSession
-    private var backgroundThread : HandlerThread? = null
-    private var backgroundHandler : Handler? = null
+    private lateinit var previewRequestBuilder: CaptureRequest.Builder
+    private lateinit var previewRequest: CaptureRequest
+    private lateinit var captureSession: CameraCaptureSession
+    private var backgroundThread: HandlerThread? = null
+    private var backgroundHandler: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,55 +53,58 @@ class MainActivity : AppCompatActivity() {
         textureView.surfaceTextureListener = surfaceTextureListener
         startBackgroundThread()
 
-        val writePermission = ContextCompat.checkSelfPermission(this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        val readPermission = ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        val writePermission =
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val readPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
 
-        if ((writePermission != PackageManager.PERMISSION_GRANTED) || (readPermission != PackageManager.PERMISSION_GRANTED)){
+        if ((writePermission != PackageManager.PERMISSION_GRANTED) || (readPermission != PackageManager.PERMISSION_GRANTED)) {
             requestStoragePermission()
         }
 
         //drawing
-        val cv : CanvasView = findViewById(R.id.canvas_view)
-        val button : ImageButton = findViewById(R.id.clear_button)
+        val cv: CanvasView = findViewById(R.id.canvas_view)
+        val button: ImageButton = findViewById(R.id.clear_button)
         button.setOnClickListener {
             cv.allDelete()
         }
 
-
     }
+
 
     @SuppressLint("MissingPermission")
     private fun openCamera() {
-        val manager : CameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val manager: CameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         try {
             //端末のどのカメラを使うか
             val cameraId: String = manager.cameraIdList[0]
-            val permission = ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)
+            val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
 
-            if(permission != PackageManager.PERMISSION_GRANTED){
+            if (permission != PackageManager.PERMISSION_GRANTED) {
                 requestCameraPermission()
                 return
             }
             //カメラ起動
-            manager.openCamera(cameraId,stateCallback,null)
-        }catch (e:Exception){
+            manager.openCamera(cameraId, stateCallback, null)
+        } catch (e: Exception) {
             e.printStackTrace()
         }
 
     }
 
     //カメラを起動する時に接続できているかを確認するCallback
-    private val stateCallback = object : CameraDevice.StateCallback(){
+    private val stateCallback = object : CameraDevice.StateCallback() {
         //カメラ接続完了
         override fun onOpened(camera: CameraDevice) {
             this@MainActivity.cameraDevice = camera
             createCameraPreviewSession()
         }
+
         //カメラ切断
         override fun onDisconnected(camera: CameraDevice) {
             camera.close()
             this@MainActivity.cameraDevice = null
         }
+
         //カメラ接続エラー
         override fun onError(camera: CameraDevice, error: Int) {
             onDisconnected(camera)
@@ -109,55 +115,60 @@ class MainActivity : AppCompatActivity() {
     private fun createCameraPreviewSession() {
         try {
             val texture = textureView.surfaceTexture
-            texture.setDefaultBufferSize(previewSize.width,previewSize.height)
+            texture.setDefaultBufferSize(previewSize.width, previewSize.height)
             val surface = Surface(texture)
             previewRequestBuilder = cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
             previewRequestBuilder.addTarget(surface)
 
             //カメラのデータをTextureViewにプレビューしている
-            cameraDevice?.createCaptureSession(Arrays.asList(surface,imageReader?.surface),
-                object : CameraCaptureSession.StateCallback(){
+            cameraDevice?.createCaptureSession(Arrays.asList(surface, imageReader?.surface),
+                object : CameraCaptureSession.StateCallback() {
                     override fun onConfigured(session: CameraCaptureSession) {
-                        if(cameraDevice == null) return
+                        if (cameraDevice == null) return
                         captureSession = session
                         try {
-                            previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
+                            previewRequestBuilder.set(
+                                CaptureRequest.CONTROL_AF_MODE,
+                                CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE
+                            )
                             previewRequest = previewRequestBuilder.build()
-                            captureSession.setRepeatingRequest(previewRequest,null,Handler(backgroundThread?.looper))
-                        }catch (e:CameraAccessException){
-                            Log.e("CameraAccessException",e.toString())
+                            captureSession.setRepeatingRequest(previewRequest, null, Handler(backgroundThread?.looper))
+                        } catch (e: CameraAccessException) {
+                            Log.e("CameraAccessException", e.toString())
                         }
                     }
+
                     override fun onConfigureFailed(session: CameraCaptureSession) {
                     }
-                },null)
-        }catch (e:CameraAccessException){
-            Log.e("CameraAccessException",e.toString())
+                }, null
+            )
+        } catch (e: CameraAccessException) {
+            Log.e("CameraAccessException", e.toString())
         }
     }
 
     //カメラ利用許可のダイヤログを表示
 
     private fun requestCameraPermission() {
-        if(shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)){
+        if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
             AlertDialog.Builder(baseContext) //android
                 .setMessage("Permission Check")
-                .setPositiveButton(android.R.string.ok){ _, _ ->
-                    requestPermissions(arrayOf(android.Manifest.permission.CAMERA),200)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 200)
                 }
-                .setNegativeButton(android.R.string.cancel){ _, _ ->
+                .setNegativeButton(android.R.string.cancel) { _, _ ->
                     finish()
                 }
-        }else{
-            requestPermissions(arrayOf(android.Manifest.permission.CAMERA),200)
+        } else {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 200)
         }
     }
 
-    private val surfaceTextureListener = object : TextureView.SurfaceTextureListener{
-       override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
-           imageReader = ImageReader.newInstance(width,height,ImageFormat.JPEG,2)
-           openCamera()
-       }
+    private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
+            imageReader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 2)
+            openCamera()
+        }
 
         override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
 
@@ -178,38 +189,61 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun requestStoragePermission(){
+    private fun requestStoragePermission() {
         //書き込み
-        if(shouldShowRequestPermissionRationale(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+        if (shouldShowRequestPermissionRationale(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
             AlertDialog.Builder(baseContext)
                 .setMessage("Permission Check")
-                .setPositiveButton(android.R.string.ok){ _,_ ->
-                    requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION_WRITE_STORAGE)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        PERMISSION_WRITE_STORAGE
+                    )
                 }
-                .setNegativeButton(android.R.string.cancel){_,_ ->
+                .setNegativeButton(android.R.string.cancel) { _, _ ->
                     finish()
                 }
                 .create()
-        }else{
+        } else {
             requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION_WRITE_STORAGE)
         }
 
         //読み込み
-        if (shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+        if (shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
             AlertDialog.Builder(baseContext)
-                    .setMessage("Permission Check")
-                .setPositiveButton(android.R.string.cancel){ _, _ ->
-                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_READ_STORAGE)
+                .setMessage("Permission Check")
+                .setPositiveButton(android.R.string.cancel) { _, _ ->
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        PERMISSION_READ_STORAGE
+                    )
                 }
-                .setNegativeButton(android.R.string.cancel){ _, _  ->
+                .setNegativeButton(android.R.string.cancel) { _, _ ->
                     finish()
                 }
                 .create()
-        }else{
+        } else {
             requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_READ_STORAGE)
         }
     }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let {
+            when (it.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    
+                }
+                MotionEvent.ACTION_MOVE -> {
 
+                }
+                MotionEvent.ACTION_UP -> {
 
+                }
+                else -> {
+
+                }
+            }
+            return true
+        } ?: return true
+    }
 }
