f9edcf40086b0f0fac81b863df91deaad0368f09
Shiori Nakayama <shiori.nakayama@pepabo.com>
2019-03-18 15:05:24 +0900

    指でなぞったら線が出るようになった

--- app/src/main/java/com/shiorin/iroiroDrawing/CanvasView.kt
+++ app/src/main/java/com/shiorin/iroiroDrawing/CanvasView.kt
@@ -11,51 +11,56 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
-class CanvasView(context: Context?, attrs:AttributeSet?) : View(context,attrs) {
-   private val pathList = ArrayList<Path>()
-    private var paint: Paint = Paint()
-    private var drawingPath = Path()
+class CanvasView @JvmOverloads constructor(
+    context: Context?, attrs:AttributeSet? = null, defStyleSttr:Int = 0) : View(context,attrs,defStyleSttr) {
 
+    var pathList = mutableListOf<Path>()
+    var paint: Paint
+    private var drawingPath : Path = Path()
 
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        paint.color = Color.RED
-        paint.strokeWidth = 10F
-        paint.style = Paint.Style.STROKE
-        //canvas!!.drawRect(300f,300f,600f,600f,paint)
-
-        for (path in pathList) {
-            canvas!!.drawPath(path, paint)
-            Log.e("Path","$path")
+    init {
+        paint = Paint().also {
+            it.color = Color.RED
+            it.style = Paint.Style.STROKE
+            it.strokeWidth = 50F
         }
     }
 
 
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        pathList.forEach {
+            canvas?.drawPath(it,paint)
+        }
+    }
 
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-
-            when(event!!.action){
-                MotionEvent.ACTION_DOWN -> {
-                   Log.e("Touch","drawingPath$drawingPath")
-                    drawingPath = Path()
-                    drawingPath.moveTo(event.x,event.y)
+        event?.let {
+            when(it.action){
+                MotionEvent.ACTION_DOWN ->{
+                    drawingPath.moveTo(it.x,it.y)
                     pathList.add(drawingPath)
+                    invalidate()
                 }
-                MotionEvent.ACTION_MOVE -> {
-                    drawingPath.moveTo(event.x,event.y)
+                MotionEvent.ACTION_MOVE ->{
+                    drawingPath.lineTo(it.x,it.y)
+                    invalidate()
                 }
                 MotionEvent.ACTION_UP ->{
+                    drawingPath.lineTo(it.x,it.y)
+                    invalidate()
+                }
+                else ->{
+
                 }
             }
-        invalidate()
-
-
-        return true
+            return true
+        }?: return true
     }
 
     fun allDelete(){
+        Log.e("delete","delete")
         pathList.clear()
     }
 
